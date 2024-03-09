c3cbe91d4cf429d258aabdc949566a409efe25a2
nguyentoann21 <nguyenvantoan28082001@gmail.com>
2024-03-09 12:23:02 +0700

    responsive footer

--- social-network-ui/src/components/footer/index.js
+++ social-network-ui/src/components/footer/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import './footer.scss';
-function Footer() {
+import PandaIcon from '../../icons/panda.png';
+
+const Footer = () => {
   return (
     <footer className="footer">
-      <ul>
-        <li><a href="/about">Giới thiệu</a></li>
-        <li><a href="/help">Trợ giúp</a></li>
-        <li><a href="/privacy">Chính sách riêng tư</a></li>
-        <li><a href="/terms">Điều khoản</a></li>
-        <li><a href="/jobs">Việc làm</a></li>
-        <li><a href="/contact">Liên hệ</a></li>
-      </ul>
-      <p>©2024 Panda Entertainment</p>
+      <div className="footer-content">
+        <div className="footer-logo">
+          <img src={PandaIcon} alt="Panda Logo" />
+          <span>Panda</span>
+        </div>
+        <div className="footer-links">
+          <Link to="/1">Terms of Service</Link>
+          <Link to="/2">Privacy Policy</Link>
+          <Link to="/3">Support</Link>
+        </div>
+        <div className="footer-social">
+          <Link to="https://facebook.com"><img src={PandaIcon} alt="Facebook"/><span>Facebook</span></Link>
+          <Link to="https://twitter.com"><img src={PandaIcon} alt="Twitter"/><span>Twitter</span></Link>
+          <Link to="https://instagram.com"><img src={PandaIcon} alt="Instagram"/><span>Instagram</span></Link>
+        </div>
+      </div>
+      <div className="footer-bottom">
+        <p>&copy; 2023 Panda. All rights reserved</p>
+      </div>
     </footer>
   );
 }
